ff7cd275cbb3049eba6f9947da7b9415c328adbb
Mirvosid <mirvosidmiraxmedov7@.com>
2023-09-27 15:01:46 +0500

    add\.bot file

---
+++ bot.py
@@ -0,0 +1,21 @@
+import telebot
+
+API_TOKEN = '6403247429:AAEBzVD7KvMvyWs0dj7Zgmu-nSTQMDUUuy0'
+
+bot = telebot.TeleBot(API_TOKEN)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\ Salom men content manager botman ya'ni ma'lum bir mavzu bo'yicha ma'lumot beraman!\
+""")
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
